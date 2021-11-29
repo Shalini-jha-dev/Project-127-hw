280346b3c41c4b9a53e1f4d558cf51ece926da63
Shalini-jha-dev <74014550+Shalini-jha-dev@users.noreply.github.com>
2021-11-29 20:04:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+song_1 = "";
+song_2 = "";
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function preload() {
+    song_1 = loadSound("Music.mp3");
+    song_2 = loadSound("Music_2.mp3");
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
